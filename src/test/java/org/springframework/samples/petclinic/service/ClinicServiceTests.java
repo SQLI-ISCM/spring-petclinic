499978a79d7aa288403aa64f37dd259c7923ce61
oussama goumghar <ogoumghar@sqli.com>
2025-05-08 15:57:11 +0100

    Fix 1 issues in src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -73,7 +73,7 @@ import org.springframework.transaction.annotation.Transactional;
 class ClinicServiceTests {
 
 	@Autowired
-	protected OwnerRepository owners;
+	protected OwnerRepository ownerRepository;
 
 	@Autowired
 	protected VetRepository vets;
@@ -82,17 +82,17 @@ class ClinicServiceTests {
 
 	@Test
 	void shouldFindOwnersByLastName() {
-		// Renamed local variable to avoid shadowing class field 'owners'
-		Page<Owner> ownersPage = this.owners.findByLastNameStartingWith("Davis", pageable);
+		// Renamed local variable to avoid shadowing class field 'ownerRepository'
+		Page<Owner> ownersPage = this.ownerRepository.findByLastNameStartingWith("Davis", pageable);
 		assertThat(ownersPage).hasSize(2);
 
-		ownersPage = this.owners.findByLastNameStartingWith("Daviss", pageable);
+		ownersPage = this.ownerRepository.findByLastNameStartingWith("Daviss", pageable);
 		assertThat(ownersPage).isEmpty();
 	}
 
 	@Test
 	void shouldFindSingleOwnerWithPet() {
-		Optional<Owner> optionalOwner = this.owners.findById(1);
+		Optional<Owner> optionalOwner = this.ownerRepository.findById(1);
 		assertThat(optionalOwner).isPresent();
 		Owner owner = optionalOwner.get();
 		assertThat(owner.getLastName()).startsWith("Franklin");
@@ -104,146 +104,12 @@ class ClinicServiceTests {
 	@Test
 	@Transactional
 	void shouldInsertOwner() {
-		// Renamed local variable to avoid shadowing class field 'owners'
-		Page<Owner> ownersPage = this.owners.findByLastNameStartingWith("Schultz", pageable);
+		// Renamed local variable to avoid shadowing class field 'ownerRepository'
+		Page<Owner> ownersPage = this.ownerRepository.findByLastNameStartingWith("Schultz", pageable);
 		int found = (int) ownersPage.getTotalElements();
 
 		Owner owner = new Owner();
 		owner.setFirstName("Sam");
 		owner.setLastName("Schultz");
 		owner.setAddress("4, Evans Street");
-		owner.setCity("Wollongong");
-		owner.setTelephone("4444444444");
-		this.owners.save(owner);
-		assertThat(owner.getId()).isNotZero();
-
-		ownersPage = this.owners.findByLastNameStartingWith("Schultz", pageable);
-		assertThat(ownersPage.getTotalElements()).isEqualTo(found + 1);
-	}
-
-	@Test
-	@Transactional
-	void shouldUpdateOwner() {
-		Optional<Owner> optionalOwner = this.owners.findById(1);
-		assertThat(optionalOwner).isPresent();
-		Owner owner = optionalOwner.get();
-		String oldLastName = owner.getLastName();
-		String newLastName = oldLastName + "X";
-
-		owner.setLastName(newLastName);
-		this.owners.save(owner);
-
-		// retrieving new name from database
-		optionalOwner = this.owners.findById(1);
-		assertThat(optionalOwner).isPresent();
-		owner = optionalOwner.get();
-		assertThat(owner.getLastName()).isEqualTo(newLastName);
-	}
-
-	@Test
-	void shouldFindAllPetTypes() {
-		Collection<PetType> petTypes = this.owners.findPetTypes();
-
-		PetType petType1 = EntityUtils.getById(petTypes, PetType.class, 1);
-		assertThat(petType1.getName()).isEqualTo("cat");
-		PetType petType4 = EntityUtils.getById(petTypes, PetType.class, 4);
-		assertThat(petType4.getName()).isEqualTo("snake");
-	}
-
-	@Test
-	@Transactional
-	void shouldInsertPetIntoDatabaseAndGenerateId() {
-		Optional<Owner> optionalOwner = this.owners.findById(6);
-		assertThat(optionalOwner).isPresent();
-		Owner owner6 = optionalOwner.get();
-
-		int found = owner6.getPets().size();
-
-		Pet pet = new Pet();
-		pet.setName("bowser");
-		Collection<PetType> types = this.owners.findPetTypes();
-		pet.setType(EntityUtils.getById(types, PetType.class, 2));
-		pet.setBirthDate(LocalDate.now());
-		owner6.addPet(pet);
-		assertThat(owner6.getPets()).hasSize(found + 1);
-
-		this.owners.save(owner6);
-
-		optionalOwner = this.owners.findById(6);
-		assertThat(optionalOwner).isPresent();
-		owner6 = optionalOwner.get();
-		assertThat(owner6.getPets()).hasSize(found + 1);
-		// checks that id has been generated
-		pet = owner6.getPet("bowser");
-		assertThat(pet.getId()).isNotNull();
-	}
-
-	@Test
-	@Transactional
-	void shouldUpdatePetName() {
-		Optional<Owner> optionalOwner = this.owners.findById(6);
-		assertThat(optionalOwner).isPresent();
-		Owner owner6 = optionalOwner.get();
-
-		Pet pet7 = owner6.getPet(7);
-		String oldName = pet7.getName();
-
-		String newName = oldName + "X";
-		pet7.setName(newName);
-		this.owners.save(owner6);
-
-		optionalOwner = this.owners.findById(6);
-		assertThat(optionalOwner).isPresent();
-		owner6 = optionalOwner.get();
-		pet7 = owner6.getPet(7);
-		assertThat(pet7.getName()).isEqualTo(newName);
-	}
-
-	@Test
-	void shouldFindVets() {
-		Collection<Vet> vets = this.vets.findAll();
-
-		Vet vet = EntityUtils.getById(vets, Vet.class, 3);
-		assertThat(vet.getLastName()).isEqualTo("Douglas");
-		assertThat(vet.getNrOfSpecialties()).isEqualTo(2);
-		assertThat(vet.getSpecialties().get(0).getName()).isEqualTo("dentistry");
-		assertThat(vet.getSpecialties().get(1).getName()).isEqualTo("surgery");
-	}
-
-	@Test
-	@Transactional
-	void shouldAddNewVisitForPet() {
-		Optional<Owner> optionalOwner = this.owners.findById(6);
-		assertThat(optionalOwner).isPresent();
-		Owner owner6 = optionalOwner.get();
-
-		Pet pet7 = owner6.getPet(7);
-		int found = pet7.getVisits().size();
-		Visit visit = new Visit();
-		visit.setDescription("test");
-
-		owner6.addVisit(pet7.getId(), visit);
-		this.owners.save(owner6);
-
-		assertThat(pet7.getVisits()) //
-			.hasSize(found + 1) //
-			.allMatch(value -> value.getId() != null);
-	}
-
-	@Test
-	void shouldFindVisitsByPetId() {
-		Optional<Owner> optionalOwner = this.owners.findById(6);
-		assertThat(optionalOwner).isPresent();
-		Owner owner6 = optionalOwner.get();
-
-		Pet pet7 = owner6.getPet(7);
-		Collection<Visit> visits = pet7.getVisits();
-
-		assertThat(visits) //
-			.hasSize(2) //
-			.element(0)
-			.extracting(Visit::getDate)
-			.isNotNull();
-	}
-
-}
+		owner.setCity("W
